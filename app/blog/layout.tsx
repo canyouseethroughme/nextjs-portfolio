8e4069fb365362feccf144725e8724b1736adae1
Stefan Andrei <32457539+canyouseethroughme@users.noreply.github.com>
2025-03-16 11:17:47 +0100

    decrease nav border to 1px

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="mt-16 mb-16 sm:mt-32 sm:mb-32 grid gap-12 sm:gap-24 grid place-content-center">
-        <div className="bg-neutral-900 opacity-90 fixed top-0 pl-2 pr-2 sm:pl-8 sm:pr-8 h-12 sm:h-16 w-[100vw] border-b-2 border-neutral-500">
+        <div className="bg-neutral-900 opacity-90 fixed top-0 pl-2 pr-2 sm:pl-8 sm:pr-8 h-12 sm:h-16 w-[100vw] border-b-[1px] border-neutral-500">
           <div className="flex justify-self-center h-full items-center justify-between w-full max-w-[2048px]">
             <Link href="/">
               <Image
